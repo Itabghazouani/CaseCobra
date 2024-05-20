9bad7cc15369f170fc3d4090a969a3ec0832866a
itab ghazouani <itab.ghazouani@gmail.com>
2024-05-20 23:10:51 +0200

    home page created

---
+++ src/components/Phone.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { HTMLAttributes } from 'react'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+
+interface IPhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string,
+  dark?: boolean
+}
+const Phone = ({ imgSrc, className, dark = false, ...props }: IPhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}>
+        <img
+          src={dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+          }
+          alt="phone image"
+          className="pointer-events-none z-50 select-none"
+        />
+        <div className="absolute -z-10 inset-0">
+          <Image
+            src={imgSrc}
+            alt="overlaying phone image"
+            className="object-cover"
+            layout='fill'
+          />
+        </div>
+    </div>
+  )
+}
+
+export default Phone
