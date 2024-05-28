b5e9d4799be976a7e91833fda8a7325b05b932f8
itab ghazouani <itab.ghazouani@gmail.com>
2024-05-29 00:09:47 +0200

    updating details

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -1,17 +1,33 @@
-import { ShippingAddress } from "@prisma/client"
-import { Body, Column, Container, Head, Heading, Hr, Html, Img, Preview, Row, Section, Text } from "@react-email/components"
+import { ShippingAddress } from '@prisma/client'
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from '@react-email/components'
 
 const OrderReceivedEmail = ({
   shippingAddress,
   orderId,
-  orderDate
+  orderDate,
 }: {
-  shippingAddress: ShippingAddress,
-  orderId: string,
+  shippingAddress: ShippingAddress
+  orderId: string
   orderDate: string
 }) => {
+  const baseUrl =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://casecobra.vercel.app'
 
-  const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://case-cobra-swart.vercel.app"
   return (
     <Html>
       <Head />
@@ -21,43 +37,55 @@ const OrderReceivedEmail = ({
           <Section style={message}>
             <Img
               src={`${baseUrl}/snake-3.png`}
-              width="65"
-              height="73"
-              alt="delivery snake"
-              style={{ margin: "auto" }} />
-            <Heading style={global.heading}>
-              Thank you for your order!
-            </Heading>
-            <Text style={global.text}>We're preparing everything for delivery and will notify you once your package has been shipped. Delivery usually takes 2 days.</Text>
-            <Text style={{ ...global.text, marginTop: 24 }}>If you have any questions regarding your order, please feel free to contact us with your order number. We're here to help.</Text>
+              width='65'
+              height='73'
+              alt='delivery snake'
+              style={{ margin: 'auto' }}
+            />
+            <Heading style={global.heading}>Thank you for your order!</Heading>
+            <Text style={global.text}>
+              We're preparing everything for delivery and will notify you once
+              your package has been shipped. Delivery usually takes 2 days.
+            </Text>
+            <Text style={{ ...global.text, marginTop: 24 }}>
+              If you have any questions regarding your order, please feel free
+              to contact us with your order number and we're here to help.
+            </Text>
           </Section>
           <Hr style={global.hr} />
           <Section style={global.defaultPadding}>
-            <Text style={adressTitle}>
-              Shipping to: {shippingAddress.name}
-            </Text>
+            <Text style={adressTitle}>Shipping to: {shippingAddress.name}</Text>
             <Text style={{ ...global.text, fontSize: 14 }}>
-              {shippingAddress.street}, {shippingAddress.city},{' '}{shippingAddress.state} {shippingAddress.postalCode},{' '}{shippingAddress.country}
+              {shippingAddress.street}, {shippingAddress.city},{' '}
+              {shippingAddress.state} {shippingAddress.postalCode}
             </Text>
           </Section>
           <Hr style={global.hr} />
           <Section style={global.defaultPadding}>
-            <Row style={{ display: "inline-flex", marginBottom: 40 }}>
+            <Row style={{ display: 'inline-flex gap-16', marginBottom: 40 }}>
               <Column style={{ width: 170 }}>
                 <Text style={global.paragraphWithBold}>Order Number</Text>
                 <Text style={track.number}>{orderId}</Text>
               </Column>
-              <Column style={{ width: 170 }}>
+              <Column style={{marginLeft: 20}}>
                 <Text style={global.paragraphWithBold}>Order Date</Text>
                 <Text style={track.number}>{orderDate}</Text>
               </Column>
             </Row>
           </Section>
+
           <Hr style={global.hr} />
+
           <Section style={paddingY}>
             <Row>
-              <Text style={{...footer.text, paddingTop: 30, paddingBottom: 30}}>
-                Please contact us if you have questions. (If you reply to this email, we won't be able to see it.)
+              <Text
+                style={{
+                  ...footer.text,
+                  paddingTop: 30,
+                  paddingBottom: 30,
+                }}>
+                Please contact us if you have any questions. (If you reply to
+                this email, we won't be able to see it.)
               </Text>
             </Row>
             <Row>
@@ -74,7 +102,6 @@ const OrderReceivedEmail = ({
 
 export default OrderReceivedEmail
 
-// Email CSS
 const paddingX = {
   paddingLeft: '40px',
   paddingRight: '40px',
