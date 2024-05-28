f28e197bd385ec149ee274dd9fd21377c7f1c579
itab ghazouani <itab.ghazouani@gmail.com>
2024-05-28 12:58:46 +0200

    payment session created, update to first deploy

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -13,12 +13,18 @@ import Confetti from "react-dom-confetti"
 import { createCheckoutSession } from "./actions"
 import { useRouter } from "next/navigation"
 import { useToast } from "@/components/ui/use-toast"
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import LoginModal from "@/components/LoginModal"
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
   const router = useRouter()
   const { toast } = useToast()
-  const [showConfetti, setShowConfetti] = useState(false)
+  const { id } = configuration
+  const { user } = useKindeBrowserClient()
+
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false)
+  const [showConfetti, setShowConfetti] = useState<boolean>(false)
 
   useEffect(() => setShowConfetti(true), [])
 
@@ -50,6 +56,15 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     }
   })
 
+  const handleCheckout = () => {
+    if (user) {
+      createPaymentSession({ configId: id})
+    } else {
+      localStorage.setItem("configurationId", id)
+      setIsLoginModalOpen(true)
+    }
+  }
+
   return (
     <>
       <div className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center" aria-hidden="true">
@@ -58,6 +73,11 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           config={{ elementCount: 200, spread: 200, }}
         />
       </div>
+
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        setIsOpen={setIsLoginModalOpen}
+      />
 
       <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="sm:cols-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
@@ -126,7 +146,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
             <div className="mt-8 flex justify-end pb-12">
               <Button
                 className="px-4 sm:px-6 lg:px-8"
-                onClick={() => createPaymentSession({ configId: configuration.id })}>
+                onClick={() => handleCheckout()}>
                 Checkout
                 <ArrowRight
                   className="h-4 w-4 ml-1.5 inline" />
